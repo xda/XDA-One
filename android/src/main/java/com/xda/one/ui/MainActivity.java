6e97d710ff5dbd9a6f11cfd602871a76048e1323
ddrager <ddrager@gmail.com>
2015-03-16 10:29:40 -0400

    Fixing crashlytics and NewRelic integrations. Newrelic is disabled by default, and crashlytics disabled by doing simpleDebug/simpleRelease build

--- android/src/main/java/com/xda/one/ui/MainActivity.java
+++ android/src/main/java/com/xda/one/ui/MainActivity.java
@@ -27,6 +27,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+// Add New Relic Reporting
+// import com.newrelic.agent.android.NewRelic;
+
 public class MainActivity extends BaseActivity
         implements NavigationDrawerFragment.Callback, SubscribedPagerFragment.Callback,
         ThreadFragment.Callback, PostPagerFragment.Callback, SearchFragment.Callback {
@@ -45,6 +48,7 @@ public class MainActivity extends BaseActivity
 
     @Override
     public void onCreate(final Bundle bundle) {
+        // Add New Relic Reporting
         /*NewRelic.withApplicationToken(
                 "AA31aa88f94b9a9db9fba799fdb1112f100438c79f"
         ).start(getApplication());*/
