c73f00698ad5e3019f6a3b1ea628c1b41e02a373
Lalit Maganti <lalitmaganti@gmail.com>
2015-04-23 16:18:43 +0100

    gradle: fix merge issues

--- android/src/main/java/com/xda/one/auth/XDAAuthenticatorActivity.java
+++ android/src/main/java/com/xda/one/auth/XDAAuthenticatorActivity.java
@@ -1,11 +1,10 @@
 package com.xda.one.auth;
 
-import com.xda.one.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+
+import com.xda.one.R;
 
 public class XDAAuthenticatorActivity extends AppCompatActivity {
 
