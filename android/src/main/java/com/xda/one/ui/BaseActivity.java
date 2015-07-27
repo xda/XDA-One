840ea80fc9b7d97c99494f1a988877a0a51f5f90
rahul kumar <rahulkumar2048@gmail.com>
2015-07-27 12:15:40 +0530

    now using AppCompat activity instead of deprecated ActionBar Activity

--- android/src/main/java/com/xda/one/ui/BaseActivity.java
+++ android/src/main/java/com/xda/one/ui/BaseActivity.java
@@ -2,9 +2,9 @@ package com.xda.one.ui;
 
 import com.xda.one.util.OneApplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public abstract class BaseActivity extends ActionBarActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     public OneApplication getOneApplication() {
         return (OneApplication) super.getApplication();
